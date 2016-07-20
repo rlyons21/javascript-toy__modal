a20575ec5a3d52b1a6be7bd8819ad81abc7b479c
Rachel Lyons <rlyons21@hotmail.com>
2016-07-19 20:06:57 -0500

    not very pretty but works

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Replace me with your JavaScript!
+window.addEventListener("load", function(){
+	var top_button = document.getElementById("topButton");
+	var cancel_button = document.getElementById("cancelButton")
+	
+	top_button.addEventListener("click", function(){
+		var add_note = document.getElementsByClassName("m");
+			add_note[0].style.display = "flex";
+	});
+
+	cancel_button.addEventListener("click", function(){
+		var add_note = document.getElementsByClassName("m");
+			add_note[0].style.display = "none";
+	});
+
+});
